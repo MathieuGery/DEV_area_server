8d58ef042860680b69afe9ecabcd33667b84b9df
mathieugery <mathieu.gery@epitech.eu>
2020-02-11 16:14:21 +0100

    [+] deploy

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+'use strict'
 
+const mongoose = require('./services/mongoose')
+const app = require('./services/express')
 
-ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    document.getElementById("root")
-);
+// start app and connect to database
+app.start()
+mongoose.connect()
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+module.exports = app
---
+++ src/routes/api/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const authRouter = require('./auth.route');
+const servicesRouter = require('./service.route');
+const aboutjson = require('../../controllers/about.controller');
+
+router.get('/status', (req, res) => {
+    res.send({status: 'OK'})
+}); // api status
+router.get('/about.json', (req, res) => {
+    try {
+        const aboutFile = require('../../about.json');
+        console.log(aboutFile)
+        aboutFile.client.host = req.ip;
+        if (aboutFile.client.host.substr(0, 7) === "::ffff:") {
+            aboutFile.client.host = aboutFile.client.host.substr(7)
+        }
+        aboutFile.server.current_time = Date.now();
+        return res.send(aboutFile);
+    } catch (error) {
+        return res.status(400).json({
+            text: "Invalid request about.json not found"
+        });
+    }
+});
+router.use('/auth', authRouter); // mount auth paths
+router.use('/services', servicesRouter);
+module.exports = router;
